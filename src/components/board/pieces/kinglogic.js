9c933320e917f511d1931e458c26c939dcf1d8ab
Thomas Lane <tlane311@gmail.com>
2020-08-31 12:03:31 -0500

    isolated king logic

---
+++ src/components/board/pieces/kinglogic.js
@@ -0,0 +1,10 @@
+function kingLogic(selection,state){
+    const position = selection.position;
+    const initial = [-9,-8,-7,-1,1,7,8,9];
+    return initial
+    .filter( number => !( selection%8===0 && (number === 7 || number === -9 || number === -1) ) ) //wall&corner detection
+    .filter( number => !( selection%8===7 && (number === -7 || number === 9 || number === 1) ) ) //wall&corner detection
+    .map( number => selection+number )
+    .filter( number => (number >=0 && number < 64) )    //keeping returned array clean of useless values
+    .filter( square => !(position[square].type !==null && position[square].color===position[selection].color));
+}
