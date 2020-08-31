2e582880ecddf64185cf9ee9340d57158ab5010a
Thomas Lane <tlane311@gmail.com>
2020-08-31 14:56:22 -0500

    improved checkDetector with .some() method

--- src/components/board/pieces/checklogic.js
+++ src/components/board/pieces/checklogic.js
@@ -5,19 +5,16 @@ export function checkDetector (state,whiteIsNext) {
     const kingPosition = state.kingPosition[color]; //gives a number in position array
     const position = state.position;
 
-    let check = false;
 
-    position.forEach(
-        (square,index) => {
-            if (square.type && square.color !== color) {
-                if (movesLogic[square.type](index,state)
-                .filter( number => number === kingPosition)
-                .length)
-                    check = check || true;
-            }
-            return null;
-        }
-    )
+    let checkCallBack = (square, index) => {
+        return square.type && square.color !== color
+        ? movesLogic[square.type](index,state)
+        .filter( number => number === kingPosition)
+        .length
+        : false;
+    }
+
+    const check = position.some( checkCallBack );
 
     return check;
 }
